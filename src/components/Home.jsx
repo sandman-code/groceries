90b81292eec138bb6073047ca72ce0ef6f7e3b0c
sandman-code <caden.crist.2019@gmail.com>
2021-07-27 21:59:04 -0400

    added base directories

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>
+                Groceries App 
+            </h1>
+                       
+        </div>
+    )
+}
